3ba81d3f90127422d00d5e1b04278e1ae7bc8b78
Kimerie <kimerie.green@gmail.com>
2016-03-29 18:47:25 -0700

    update

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -5,29 +5,32 @@ var SongQueue = Songs.extend({
     //start enqueue method
 
     this.on('enqueue', function(song) {
-      this.get(this).add(song)
+      this.get(this).add(song);
     });
 
-    this.on('dequeue', function() {
-      this.remove(this.at(0))
-
+    this.on('dequeue', function(song) {
+      if(this.at(0) === song){
+        this.playNext();
+      } else{
+        this.remove(this.at(0));
+      }
     });
 
     this.on('add', function() {
       if (this.at(1) === undefined){
         this.playFirst();
-      } 
+      }
     });
 
     this.on('ended', function() {
-      this.remove(this.at(0))
+      this.remove(this.at(0));
       if (this.length > 0){
         this.playFirst();
-      } 
-      
+      }
+
     });
 
-  }, 
+  },
 
 
   playFirst: function(){
